aabe97cc8451a908fa15f68d7a690510e4467b6e
m31271n <m31271n@users.noreply.github.com>
2018-10-08 00:26:23 +0800

    update

--- src/index.js
+++ src/index.js
@@ -1,84 +1,58 @@
-import {
-  Black,
-  CanvasDriver,
-  Input,
-  TextField,
-  GameObject,
-  StageScaleMode,
-  StageOrientation,
-  SplashScreen,
-} from 'black'
+import * as PIXI from 'pixi.js'
 import videoAddress from 'Assets/video.mp4'
 
-class Game extends GameObject {
-  constructor() {
-    super()
+const containerElement = document.querySelector('#game-container')
 
-    SplashScreen.enabled = false
-    Black.stage.scaleMode = StageScaleMode.LETTERBOX
-    Black.stage.setSize(1500, 750)
-    Black.stage.orientation = StageOrientation.PORTRAIT
-    this.touchable = true
-  }
+// PIXI BUTTON
+const app = new PIXI.Application()
+containerElement.appendChild(app.view)
+const buttonCanvas = new PIXI.Text('BUTTON - CANVAS', {
+  fill: ['#ffffff', '#00ff99'],
+})
+buttonCanvas.x = 50
+buttonCanvas.y = 300
+buttonCanvas.interactive = true
+app.stage.addChild(buttonCanvas)
+buttonCanvas.on('pointerup', () => playVideo())
 
-  onAdded() {
-    this.buttonCanvas = new TextField('BUTTON - CANVAS')
-    this.buttonCanvas.size = 60
-    this.buttonCanvas.color = 0xff0000
-    this.buttonCanvas.align = 'center'
-    this.buttonCanvas.alignAnchor()
-    this.buttonCanvas.x = this.stage.centerX
-    this.buttonCanvas.y = this.stage.centerY
-    this.addChild(this.buttonCanvas)
-    this.buttonCanvas.touchable = true
-    this.buttonCanvas.on('pointerUp', () => this.playVideo())
+// DOM BUTTON
+const button = document.createElement('div')
+button.innerHTML = 'BUTTON - DOM'
+button.style.position = 'absolute'
+button.style.top = '0'
+button.style.left = '0'
+button.style.marginTop = '40%'
+button.style.width = '100%'
+button.style.textAlign = 'center'
+button.style.color = 'green'
+button.style.fontSize = '30px'
+containerElement.appendChild(button)
+button.addEventListener('touchend', () => playVideo())
 
-    const button = document.createElement('div')
-    button.innerHTML = 'BUTTON - DOM'
-    button.style.position = 'absolute'
-    button.style.top = '0'
-    button.style.left = '0'
-    button.style.marginTop = '40%'
-    button.style.width = '100%'
-    button.style.textAlign = 'center'
-    button.style.color = 'green'
-    button.style.fontSize = '30px'
-    const { containerElement } = Black.instance
-    containerElement.appendChild(button)
-    button.addEventListener('touchend', () => this.playVideo())
-  }
+function playVideo() {
+  const video = document.createElement('video')
+  video.src = videoAddress
+  video.style.position = 'absolute'
+  video.style.top = '0'
+  video.style.left = '0'
+  video.style.width = '200px'
+  video.style.height = '200px'
+  video.style.zIndex = 2
+  video.setAttribute('preload', 'auto')
+  video.setAttribute('playsinline', '')
 
-  playVideo() {
-    const video = document.createElement('video')
-    video.src = videoAddress
-    video.style.position = 'absolute'
-    video.style.top = '0'
-    video.style.left = '0'
-    video.style.width = '200px'
-    video.style.height = '200px'
-    video.style.zIndex = 2
-    video.setAttribute('preload', 'auto')
-    video.setAttribute('playsinline', '')
+  // WebKit-based browser adaptation
+  video.setAttribute('webkit-playsinline', '')
 
-    // WebKit-based browser adaptation
-    video.setAttribute('webkit-playsinline', '')
+  // Tencent x5 adaptation
+  video.setAttribute('x5-playsinline', '')
+  video.setAttribute('x5-video-player-type', 'h5')
 
-    // Tencent x5 adaptation
-    video.setAttribute('x5-playsinline', '')
-    video.setAttribute('x5-video-player-type', 'h5')
+  containerElement.appendChild(video)
 
-    const { containerElement } = Black.instance
-    containerElement.appendChild(video)
-
-    const promise = video.play()
-    promise.then(_ => {}).catch(error => {
-      alert('Autoplay was prevented. check the console.')
-      console.error(error)
-    })
-  }
+  const promise = video.play()
+  promise.then(_ => {}).catch(error => {
+    alert('Autoplay was prevented. check the console.')
+    console.error(error)
+  })
 }
-
-const black = new Black('game-container', Game, CanvasDriver, [Input])
-black.start()
-black.pauseOnBlur = false
-black.pauseOnHide = false
